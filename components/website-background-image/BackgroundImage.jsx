c34e237601f464d25047152f31a8852457f35b8f
Kunuk Nykjær <kunukn@gmail.com>
2019-11-05 09:09:46 +0100

    fix background

--- components/website-background-image/BackgroundImage.jsx
+++ components/website-background-image/BackgroundImage.jsx
@@ -1,21 +1,21 @@
-import { useStore, Subscribe } from "laco-react";
+import { useStore, Subscribe } from 'laco-react'
 
-import store, { dispatchResize } from "~/store.js";
+import store, { dispatchResize } from '~/store.js'
 
 const BackgroundImage = props => {
-  let { windowHeight, windowWidth } = useStore(store);
+  let { windowHeight, windowWidth } = useStore(store)
 
   React.useEffect(() => {
     //dispatchResize();
-  }, []);
+  }, [])
 
   return (
     <>
       <div
-        style={{ height: `${windowHeight ? windowHeight : ""}px` }}
-        className={props.isEnabled ? "fixed-background" : "fixed"}
+        style={{ height: `${windowHeight ? windowHeight : ''}px` }}
+        className={props.isEnabled ? 'fixed-background' : 'fixed'}
       >
-        <div className="image-background"></div>
+        <div className='image-background'></div>
       </div>
       <style jsx>{`
         .fixed {
@@ -32,25 +32,23 @@ const BackgroundImage = props => {
           height: 100%;
           width: 100%;
           overflow: hidden;
-          background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9))
+          background: #111;
+          background: linear-gradient(rgba(black, 0.8), rgba(black, 0.96));
         }
         .image-background {
           position: absolute;
           top: 0;
           left: 0;
-          /* https://stackoverflow.com/questions/23208200/how-to-darken-a-background-using-css */
-          background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)),
-            url($backgroundImageUrl) no-repeat 50% 50% / contain;
-
+          background: url($backgroundImageUrl) no-repeat 50% 50% / contain;
           width: 1024px;
           width: 100%;
           height: 683px;
-          height: 100%;
+          _height: 100%;
           _opacity: 0.5;
         }
       `}</style>
     </>
-  );
-};
+  )
+}
 
-export default BackgroundImage;
+export default BackgroundImage
