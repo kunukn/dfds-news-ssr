6b54d4e485479c7f10241e3ce34b478ff3f71ff4
Kunuk Nykjær <kunyk@dfds.com>
2019-11-06 15:50:20 +0100

    faster font

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,6 @@ export default class MyDocument extends Document {
           />
           <meta name='description' content='DFDS news' />
           <meta name='keywords' content='DFDS news' />
-          <link rel='canonical' href='https://shipping-news.tech' />
           <link
             rel='preload'
             href='https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/DFDS-Regular.woff2'
@@ -49,18 +48,52 @@ export default class MyDocument extends Document {
             type='font/woff2'
             crossOrigin='anonymous'
           />
-          <link
-            rel='stylesheet'
-            href='https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/font.css'
-          />
-          <link
-            rel='stylesheet'
-            href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
-          />
+          <style jsx global>{`
+            @font-face {
+              font-display: swap;
+              font-family: DFDS;
+              src: url('https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/DFDS-Regular.woff2')
+                  format('woff2'),
+                url('https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/DFDS-Regular.woff')
+                  format('woff');
+              font-weight: normal;
+            }
+            @font-face {
+              font-display: swap;
+              font-family: DFDS;
+              src: url('https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/DFDS-Bold.woff2')
+                  format('woff2'),
+                url('https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/DFDS-Bold.woff')
+                  format('woff');
+              font-weight: bold;
+            }
+            @font-face {
+              font-display: swap;
+              font-family: DFDS;
+              src: url('https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/DFDS-Light.woff2')
+                  format('woff2'),
+                url('https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/DFDS-Light.woff')
+                  format('woff');
+              font-weight: 300;
+            }
+            @font-face {
+              font-display: swap;
+              font-family: DFDS;
+              src: url('https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/DFDS-Italic.woff2')
+                  format('woff2'),
+                url('https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/DFDS-Italic.woff')
+                  format('woff');
+              font-style: italic;
+            }
+          `}</style>
         </Head>
         <body>
           <Main />
           <NextScript />
+          <link
+            rel='stylesheet'
+            href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
+          />
           <link
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,400i,700&display=swap'
             rel='stylesheet'
