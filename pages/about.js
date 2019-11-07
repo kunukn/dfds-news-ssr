790fb1c73ee18f3e3d36cfe3b660df2a11cdf807
Kunuk Nykjær <kunyk@dfds.com>
2019-11-07 07:49:13 +0100

    add about

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <>
+      <div>
+        <h1>DFSD News app</h1>
+      </div>
+      <style jsx>{`
+        h1 {
+          color: $color-groupBlue;
+          text-align: center;
+          margin: 0;
+          padding: 10px;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default About
