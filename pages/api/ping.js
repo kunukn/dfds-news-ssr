f6b0066953e2cdf58828f37d89c05fe68bf8c63b
Kunuk Nykjær <kunukn@gmail.com>
2019-11-14 22:02:23 +0100

    remove unused

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -1,10 +1,10 @@
 const data = 'pong'
 
 export default (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
+  // res.header('Access-Control-Allow-Origin', '*')
+  // res.header(
+  //   'Access-Control-Allow-Headers',
+  //   'Origin, X-Requested-With, Content-Type, Accept'
+  // )
   return res.json(data)
 }
