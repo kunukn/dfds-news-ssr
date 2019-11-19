0a9ef158dda477b0026fb3374aac6220801dba17
kunuk <kunyk@dfds.com>
2019-11-19 09:02:23 +0100

    bump update

--- mock/mock-server.js
+++ mock/mock-server.js
@@ -1,6 +1,7 @@
 // server.js
 const path = require('path')
-const pause = require('connect-pause');
+const pause = require('connect-pause')
+const argv = require('minimist')(process.argv.slice(2))
 
 const news = require('../data-layer/news')
 const news10 = require('../data-layer/news-10')
@@ -10,6 +11,8 @@ const article3 = require('../data-layer/article-3')
 
 const jsonServer = require('json-server')
 
+console.log('** argv **', argv)
+
 const server = jsonServer.create()
 
 const router = jsonServer.router({
@@ -17,12 +20,16 @@ const router = jsonServer.router({
   'mock-news-10': news10,
   'mock-article-1': article1,
   'mock-article-2': article2,
-  'mock-article-3': article3,
+  'mock-article-3': article3
 })
 const middlewares = jsonServer.defaults()
 
+let delay = argv.delay ? argv.delay : 0
+
+console.log('** delay **', delay)
+
 server.use(middlewares)
-//server.use(pause(3000));
+server.use(pause(delay))
 server.use('/api', router)
 server.use(router)
 server.listen(8008, () => {
